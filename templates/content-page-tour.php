c6228a9a6af3521038c22f09dbf1973d06b9a613
Michael Shoup <michael@12southmusic.com>
2014-05-16 16:48:57 -0400

    update repo

---
+++ templates/content-page-tour.php
@@ -0,0 +1,17 @@
+<div class="col-xs-3 bands-in-town-section">
+				<h1 class="bands-in-town-h1">Chuck on Tour</h1>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-12 bands-in-town-embed">
+			<script type='text/javascript' src='http://www.bandsintown.com/javascripts/bit_widget.js'></script>
+<a href='http://www.bandsintown.com' class='bit-widget-initializer' data-display-limit="4" data-text-color="none" data-artist='Chuck Wicks'>Bandsintown</a>
+			</div>
+		</div>
+        <div class="row">
+			<div class="col-md-12">
+				<button type="button" onclick="window.location.href='/tour'" href="/tour">More Dates</button>
+			</div>
+		</div>
+	</div>
+</div>
---
+++ templates/sidenavbar.php
@@ -0,0 +1,20 @@
+<div class="sidenav navbar navbar-inverse" data-spy="affix" role="sidenav">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle navbar" data-toggle="collapse" data-target=".navbar-collapse">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+    </div>
+      <div class="collapse navbar-collapse affix" role="navigation" >
+      <div class="navlogo">
+      <a href="/index.php"><img src="/assets/img/cwlogo.png">
+      </div>     
+      <?php
+        if (has_nav_menu('side_navigation')) :
+            wp_nav_menu(array('theme_location' => 'side_navigation', 'menu_class' => 'nav nav-pills nav-stacked'));
+        endif;
+        ?>
+      </div>
+</div>
