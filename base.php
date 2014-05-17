7d25279fb741267041c05c56f2ce857455e5036b
benhardt <ben@ben-hardt.com>
2014-05-17 16:55:07 -0500

    gh dumb

--- base.php
+++ base.php
@@ -32,15 +32,12 @@
        <div class="main <?php echo roots_main_class(); ?>" role="main">
         <?php include roots_template_path(); ?>
       </div><!-- /.main -->
-<<<<<<< HEAD
       <?php if (roots_display_sidenavbar()) : ?>
             <div class="<?php echo roots_sidenavbar_class(); ?>">
                 <section class="side-nav-wrapper">
                     <?php get_template_part('templates/sidenavbar'); ?>
                 </section>
             </div><!-- /.sidenavbar -->
-=======
->>>>>>> 335e5006c8c69c1a02115f8c6cce427d6d4a6ede
       <!--<?php if (roots_display_sidebar()) : ?>
         <aside class="sidebar <?php echo roots_sidebar_class(); ?>" role="complementary">
           <?php include roots_sidebar_path(); ?>
