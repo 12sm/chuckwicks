8a229219832a3413f6d17c8b7ad0af506bb590d0
benhardt <ben@ben-hardt.com>
2014-05-17 15:46:46 -0500

    u

--- base.php
+++ base.php
@@ -12,10 +12,10 @@
     // Use Bootstrap's navbar if enabled in config.php
     if (current_theme_supports('bootstrap-top-navbar')) {
       get_template_part('templates/header-top-navbar');
-    } else {-->
+    } else {
 
     }
-  ?>
+  ?>-->
 
   <div class="wrap container" role="document">
     <div class="content row">
