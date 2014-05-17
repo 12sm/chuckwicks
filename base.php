c44c03df4cc1bebb6b32afcf4b229072e017da7a
benhardt <ben@ben-hardt.com>
2014-05-17 16:57:27 -0500

    wow

--- base.php
+++ base.php
@@ -32,12 +32,6 @@
        <div class="main <?php echo roots_main_class(); ?>" role="main">
         <?php include roots_template_path(); ?>
       </div><!-- /.main -->
-      <?php if (roots_display_sidenavbar()) : ?>
-            <div class="<?php echo roots_sidenavbar_class(); ?>">
-                <section class="side-nav-wrapper">
-                    <?php get_template_part('templates/sidenavbar'); ?>
-                </section>
-            </div><!-- /.sidenavbar -->
       <!--<?php if (roots_display_sidebar()) : ?>
         <aside class="sidebar <?php echo roots_sidebar_class(); ?>" role="complementary">
           <?php include roots_sidebar_path(); ?>
