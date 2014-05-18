02cc0169a0238e5a6c2b95cf42b093796d0b63c2
benhardt <ben@ben-hardt.com>
2014-05-18 11:34:07 -0500

    work!

--- base.php
+++ base.php
@@ -25,16 +25,16 @@
                 </section>
             </div><!-- /.sidenavbar -->
         <?php endif; ?>
-        <?php  if ( is_front_page() ) { ?>
+        <!--<?php  if ( is_front_page() ) { ?>
             <?php get_template_part('templates/header'); ?>
             <div class="mainhome <?php echo roots_main_class(); ?>" role="main">
             <?php include roots_template_path(); ?>
             </div>  
-        <?php } else { ?>
+        <?php } else { ?>-->
             <div class="main <?php echo roots_main_class(); ?>" role="main">
             <?php include roots_template_path(); ?>
             </div>
-        <? } ?>
+        <!--<? } ?>-->
         <!-- /.main -->
       <!--<?php if (roots_display_sidebar()) : ?>
         <aside class="sidebar <?php echo roots_sidebar_class(); ?>" role="complementary">
