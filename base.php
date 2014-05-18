c4faf3882d49c60db37047914db8bc825893452e
benhardt <ben@ben-hardt.com>
2014-05-17 19:13:29 -0500

    ch

--- base.php
+++ base.php
@@ -26,7 +26,7 @@
             </div><!-- /.sidenavbar -->
         <?php endif; ?>
         <?php  if ( is_front_page() )    { ?>
-            get_template_part('templates/header'); 
+            <?get_template_part('templates/header'); ?>
             <div class="home <?php echo roots_main_class(); ?>" role="main">
             </div>
         <?php } else { ?>
