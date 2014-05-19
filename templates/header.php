f41b4b4522f467fb27d46c6aa9c3871d4095eeed
benhardt <ben@ben-hardt.com>
2014-05-19 16:02:43 -0500

    logo resive fix

--- templates/header.php
+++ templates/header.php
@@ -1,4 +1,4 @@
-<div class="mainlogo"><img src="/assets/img/cwname.png"></div>
+<div class="img-responsive"><img src="/assets/img/cwname.png"></div>
 <!--<header class="banner container" role="banner">
   <div class="row">
     <div class="col-xs-12">
