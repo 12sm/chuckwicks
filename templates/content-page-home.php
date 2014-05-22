b035b162dadc1060909d5dada0bd85f13e8005cb
benhardt <ben@ben-hardt.com>
2014-05-22 13:24:52 -0500

    Revert "shortcode"
    This reverts commit 56736cae7162e6b9dfd835ef91b847ea0e38b1af.

--- templates/content-page-home.php
+++ templates/content-page-home.php
@@ -2,9 +2,7 @@
 		<div class="row">   
              <div class="mainlogo"><img src="/assets/img/cwname.png" class="img-responsive"></div>
              	<div class="maincontent">
-             		<?php
-					echo do_shortcode('[wpv-view name="Featured News"]');
-					 ?>
+             		[wpv-view name="Featured News"]
              	</div>
          	</div>
          </div>
