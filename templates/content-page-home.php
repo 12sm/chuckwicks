56736cae7162e6b9dfd835ef91b847ea0e38b1af
benhardt <ben@ben-hardt.com>
2014-05-22 13:19:44 -0500

    shortcode

--- templates/content-page-home.php
+++ templates/content-page-home.php
@@ -2,7 +2,9 @@
 		<div class="row">   
              <div class="mainlogo"><img src="/assets/img/cwname.png" class="img-responsive"></div>
              	<div class="maincontent">
-             		[wpv-view name="Featured News"]
+             		<?php
+					echo do_shortcode('[wpv-view name="Featured News"]');
+					 ?>
              	</div>
          	</div>
          </div>
