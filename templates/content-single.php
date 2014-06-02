d58161bb7da1cac77a2bed803182c33df6ff8f72
michael <michael@12southmusic.com>
2014-06-02 17:41:56 -0500

    Add share buttons :)

--- templates/content-single.php
+++ templates/content-single.php
@@ -2,6 +2,14 @@
   <article <?php post_class(); ?>>
     <header>
       <h2 class="entry-title"><?php the_title(); ?></h2>
+<div class="addthis_toolbox addthis_default_style addthis_32x32_style">
+<a class="addthis_button_facebook"></a>
+<a class="addthis_button_twitter"></a>
+<a class="addthis_button_pinterest_share"></a>
+<a class="addthis_button_google_plusone_share"></a>
+<a class="addthis_button_compact"></a><a class="addthis_counter addthis_bubble_style"></a>
+</div>
+<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-538cfcf1728d4a2d"></script>
       <div class="newsdiv">
       <center><img src="/assets/img/hr.png" width="650px"></center>
     </div>
