4804f8fd506f875ee4299cfe473206a025d1fd9c
michael <michael@12southmusic.com>
2014-06-02 17:44:36 -0500

    Smaller buttons

--- templates/content-single.php
+++ templates/content-single.php
@@ -2,14 +2,15 @@
   <article <?php post_class(); ?>>
     <header>
       <h2 class="entry-title"><?php the_title(); ?></h2>
-<div class="addthis_toolbox addthis_default_style addthis_32x32_style">
+<div class="addthis_toolbox addthis_default_style addthis_16x16_style">
 <a class="addthis_button_facebook"></a>
 <a class="addthis_button_twitter"></a>
 <a class="addthis_button_pinterest_share"></a>
 <a class="addthis_button_google_plusone_share"></a>
-<a class="addthis_button_compact"></a><a class="addthis_counter addthis_bubble_style"></a>
+<a class="addthis_button_compact"></a>
 </div>
 <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-538cfcf1728d4a2d"></script>
+<time class="published" datetime="<?php echo get_the_time('c'); ?>"><?php echo get_the_date(); ?></time>
       <div class="newsdiv">
       <center><img src="/assets/img/hr.png" width="650px"></center>
     </div>
