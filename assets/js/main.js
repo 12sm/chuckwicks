55b4389d95b8a8aa30430d6a9bd00c54e8fa92a5
benhardt <ben@ben-hardt.com>
2014-08-21 08:54:13 -0500

    skin navbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -119,8 +119,29 @@ var UTIL = {
   
 
 $(document).ready(UTIL.loadEvents);
+        $(document).ready(function() {
+        
+            $('#nav').affix({
+      offset: {
+        top: $('header').height()
+      }
+}); 
+          $('#nav').on('affix.bs.affix', function () {
+            var navHeight = $('.navbar').outerHeight(true);
+            
+          $('#nav + .container').css('margin-top', navHeight);
+    });
+          $('#nav').on('affix-top.bs.affix', function () {
+             $('#nav + .container').css('margin-top', 0);
+    });
 
-
+          $('#sidebar').affix({
+            offset: {
+            top: 17
+             }
+}); 
+        
+        });
 
 })(jQuery); // Fully reference jQuery after this point.
 
