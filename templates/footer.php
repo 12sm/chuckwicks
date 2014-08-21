6b926d3fc7228bb2e8b42f4f71e50ccec09153f8
benhardt <ben@ben-hardt.com>
2014-08-21 08:54:56 -0500

    Revert "skin navbar"
    This reverts commit 55b4389d95b8a8aa30430d6a9bd00c54e8fa92a5.

--- templates/footer.php
+++ templates/footer.php
@@ -8,7 +8,21 @@
             endif;
           ?>
     </div>
-    <div id="social-float" class="row social-nav">
+         
+    <div class="row signup-row">
+        <div id="signup">
+          <div class="row">
+            <div class="col-sm-3 topspin-signup" id="signup-button" type="text" placeholder="Name:">
+              <h3>&nbsp; JOIN THE<br>MAILING LIST</h3>
+            </div>  
+            <div class="col-sm-6 topspin-signup">
+              <link href="https://app.e2ma.net/css/signup.sml.css" rel="stylesheet" type="text/css"><script type="text/javascript" src="https://app.e2ma.net/app2/audience/tts_signup/1762603/8258488ac6fb4eb4b4a6cd35bd68c9c0/1736041/?v=a"></script><div id="load_check" class="signup_form_message" >This form needs Javascript to display, which your browser doesn't support. <a href="https://app.e2ma.net/app2/audience/signup/1762603/1736041/?v=a"> Sign up here</a> instead </div><script type="text/javascript">signupFormObj.drawForm();</script>
+            </div>      
+        </div>
+    </div>
+  </div>
+  
+  <div id="social-float" class="row social-nav">
     <ul id="menu-social-navigation" class="">
       <li class="menu-"><a href="https://www.facebook.com/chuckwicks" target="_blank"><i class="fa fa-facebook fa-2x"></i></a></li>
       <li class="menu-"><a href="http://instagram.com/chuckwicks" target="_blank"><i class="fa fa-instagram fa-2x"></i></a></li>
@@ -17,18 +31,8 @@
       <li class="menu-"><a href="http://smarturl.it/usagain" target="_blank"><img src="/assets/img/itunes.png" onmouseover="this.src='/assets/img/itunes-active.png'" onmouseout="this.src='/assets/img/itunes.png'"></a></i></a></li>
     </ul>
     </div>
-
-    <div class="col-xs-8 signup-row">
-    <div class="signup">
-    <div id="mc_embed_signup"><form action="http://christinebrownpiano.us6.list-manage.com/subscribe/post?u=e8eddda6386725bbc2074242b&amp;id=994868238f" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate=""> <label for="mce-EMAIL">JOIN THE MAILING LIST</label>
-      <div class="input-container"> <input value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required="" type="email"> <input value="Signup" name="subscribe" id="mc-embedded-subscribe" class="button" type="submit">
-      </div>
-      <div style="position: absolute; left: -5000px;"> <input name="b_e8eddda6386725bbc2074242b_994868238f" value="" type="text">
-      </div>
-    </form>
-    </div>             
-   </div>          
-    <div class="col-xs-4 credits"><p><center>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>  |  <a href="http://12southmusic.com/" target="_blank">built by 12SM</a></center></p>
+     <div class="row">         
+    <div class="col-xs-2 credits"><p><center>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>  |  <a href="http://12southmusic.com/" target="_blank">built by 12SM</a></center></p>
     </div>
     </div>
     </div>
